e9979f721efde433c79bc7e4be5f94e047571bdc
Trixesu <trixteresu@gmail.com>
2024-12-12 02:14:12 +0700

    Update UAS_245314018.java

--- UAS_245314018.java
+++ UAS_245314018.java
@@ -2,44 +2,65 @@ package uas_245314018;
 import java.util.Scanner;
 public class UAS_245314018 {
     public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.print("Masukkan Jumlah Mahasiswa : ");
-    int n = input.nextInt();
-    String nama = "";
-    double uts1 = 0, uts2 = 0, uas = 0;
-    
-    for (int i = 1; i <= n; i++) {
-        System.out.print("Nama        : ");
-        nama = input.next();
-        System.out.print("Nilai UTS 1 : ");
-        uts1 = input.nextInt();
-        System.out.print("Nilai UTS 2 : ");
-        uts2 = input.nextInt();
-        System.out.print("Nilai UAS   : ");
-        uas = input.nextInt();
-        System.out.println();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int n = input.nextInt();
+        input.nextLine();
+
+        String[] nama = new String[n];
+        double[] nilaiFinal = new double[n];
+        int[] distribusi = new int[5];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan nama mahasiswa ke-" + (i + 1) + " : ");
+            nama[i] = input.nextLine();
+
+            System.out.print("Masukkan nilai UTS 1 : ");
+            double uts1 = input.nextDouble();
+
+            System.out.print("Masukkan nilai UTS 2 : ");
+            double uts2 = input.nextDouble();
+
+            System.out.print("Masukkan nilai UAS : ");
+            double uas = input.nextDouble();
+            input.nextLine();
+            
+            nilaiFinal[i] = 0.3 * uts1 + 0.3 * uts2 + 0.4 * uas;
+            
+            char huruf = konversi(nilaiFinal[i]);
+            if (huruf == 'A') distribusi[0]++;
+            else if (huruf == 'B') distribusi[1]++;
+            else if (huruf == 'C') distribusi[2]++;
+            else if (huruf == 'D') distribusi[3]++;
+            else if (huruf == 'E') distribusi[4]++;
+        }
+
+        System.out.println("\nDaftar Nilai Mahasiswa");
+        System.out.println("-------------------------------------------");
+        System.out.println("No.\tNama\t\tNilai\t\tHuruf");
+        System.out.println("===========================================");
+
+        for (int i = 0; i < n; i++) {
+            System.out.println((i + 1)+"\t"+nama[i]+"\t\t"+nilaiFinal[i]+"\t\t"+konversi(nilaiFinal[i]));
+        }
+
+        System.out.println("-------------------------------------------");
+
+        System.out.println("Distribusi Nilai");
+        System.out.println("A = " + distribusi[0] + " mahasiswa");
+        System.out.println("B = " + distribusi[1] + " mahasiswa");
+        System.out.println("C = " + distribusi[2] + " mahasiswa");
+        System.out.println("D = " + distribusi[3] + " mahasiswa");
+        System.out.println("E = " + distribusi[4] + " mahasiswa");
+
+        input.close();
     }
-        double nilaiFinal = (0.3 * uts1) + (0.3 * uts2) + (0.4 * uas);
-        
-        String grade;
-        if (nilaiFinal >= 80) {
-           grade = "A";
-        } else if (65 <= nilaiFinal && nilaiFinal < 80) {
-           grade = "B";
-        } else if (55 <= nilaiFinal && nilaiFinal < 65) {
-           grade = "C";
-        } else if (45 <= nilaiFinal && nilaiFinal < 45) {
-            grade = "D";
-        } else {
-            grade = "E";
-        }
-        System.out.println("--------------------------------------");
-        System.out.println("No.\tNama\t\tNilai\tGrade");
-        System.out.println("======================================");
-        for (int j = 1; j <= n; j++) {
-            System.out.println(j+"\t"+nama+"\t\t"+(int)nilaiFinal+"\t"+grade);
-        }
-        System.out.println("--------------------------------------");
-    input.close();
+    public static char konversi(double nilaiFinal) {
+        if (nilaiFinal >= 80) return 'A';
+        else if (nilaiFinal >= 65) return 'B';
+        else if (nilaiFinal >= 55) return 'C';
+        else if (nilaiFinal >= 45) return 'D';
+        else return 'E';
     }
 }
